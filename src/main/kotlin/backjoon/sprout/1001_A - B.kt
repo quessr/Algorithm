b546f2a4323f671d3cb95cd7a0324075f1671480
quessr <heosera76@gmail.com>
2024-12-02 22:58:18 +0900

    A-B

---
+++ src/main/kotlin/backjoon/sprout/1001_A - B.kt
@@ -0,0 +1,17 @@
+package backjoon.sprout
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val (a, b) = br.readLine().split(' ').map(String::toInt)
+    bw.write("${a - b}")
+
+    bw.flush()
+    bw.close()
+}
