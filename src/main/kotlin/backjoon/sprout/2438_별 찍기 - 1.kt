34d1f270e816c256a20d7625052c5227a1a0b80f
quessr <heosera76@gmail.com>
2024-12-05 22:48:16 +0900

    별 찍기 - 1

---
+++ src/main/kotlin/backjoon/sprout/2438_별 찍기 - 1.kt
@@ -0,0 +1,20 @@
+package backjoon.sprout
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val a = br.readLine().toInt()
+
+    for (i in 1..a) {
+        bw.write("*".repeat(i) + "\n")
+    }
+
+    bw.flush()
+    bw.close()
+}
