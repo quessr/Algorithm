542eb84c1e8329f0b60c6d656bd7c836019c35a7
quessr <heosera76@gmail.com>
2025-01-05 17:04:48 +0900

    좌표 정렬하기 2

---
+++ src/main/kotlin/backjoon/silver/11651_좌표 정렬하기 2.kt
@@ -0,0 +1,21 @@
+package backjoon.silver
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    val n = br.readLine().toInt()
+    val inputList = mutableListOf<Pair<Int, Int>>()
+
+    repeat(n) {
+        val (x, y) = br.readLine().split(" ").map { it.toInt() }
+        inputList.add(Pair(x, y))
+    }
+
+    val sortedList = inputList.sortedWith(compareBy({ it.second }, { it.first }))
+
+    bw.write(sortedList.joinToString("\n") { "${it.first} ${it.second}" })
+
+    bw.flush()
+    bw.close()
+}
