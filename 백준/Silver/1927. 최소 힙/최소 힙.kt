702e155d7f62271dcf0f77ce16faddd7d273488e
Sera Heo <quessr00@gmail.com>
2025-02-24 16:00:40 +0900

    [Silver II] Title: 최소 힙, Time: 272 ms, Memory: 27144 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.kt
@@ -0,0 +1,26 @@
+import java.util.PriorityQueue
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    val n = br.readLine().toInt()
+    val pq = PriorityQueue<Int>()
+
+    repeat(n) {
+        val num = br.readLine().toInt()
+
+        if (num == 0) {
+            if (pq.isEmpty()) {
+                bw.write("0\n")
+            } else {
+                bw.write("${pq.poll()}\n")
+            }
+        } else {
+            pq.offer(num)
+        }
+    }
+
+    bw.flush()
+    bw.close()
+}
