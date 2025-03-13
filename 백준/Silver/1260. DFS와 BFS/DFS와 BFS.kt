5434e894a8e541faaa0267379132442d577b1ece
Sera Heo <quessr00@gmail.com>
2025-03-13 16:51:34 +0900

    [Silver II] Title: DFS와 BFS, Time: 224 ms, Memory: 22980 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.kt
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.kt
@@ -1,63 +1,72 @@
-package backjoon.silver
-
 import java.util.LinkedList
 import java.util.Queue
 
 private lateinit var graph: Array<MutableList<Int>>
+private lateinit var visited: BooleanArray
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
-
-    fun dfs(node: Int, graph: Array<MutableList<Int>>, visited: BooleanArray) {
-        visited[node] = true
-        bw.write("$node ")
-
-        for (next in graph[node]) {
-            if (!visited[next]) {
-                dfs(next, graph, visited)
-            }
-        }
-    }
-
-    fun bfs(start: Int, graph: Array<MutableList<Int>>, visited: BooleanArray) {
-        val queue: Queue<Int> = LinkedList()
-        queue.add(start)
-        visited[start] = true
-
-        while (queue.isNotEmpty()) {
-            val current = queue.poll()
-            bw.write("$current ")
-
-            for (next in graph[current]) {
-                if (!visited[next]) {
-                    queue.add(next)
-                    visited[next] = true
-                }
-            }
-        }
-    }
 
     val (n, m, v) = br.readLine().split(" ").map { it.toInt() }
 
     graph = Array(n + 1) { mutableListOf() }
 
     repeat(m) {
-        val (a, b) = br.readLine().split(" ").map { it.toInt() }
-        graph[a].add(b)
-        graph[b].add(a)
+        val (from, to) = br.readLine().split(" ").map { it.toInt() }
+        graph[from].add(to)
+        graph[to].add(from)
     }
 
     for (i in 1..n) {
         graph[i].sort()
     }
 
-    val visitedDFS = BooleanArray(n + 1)
-    dfs(v, graph, visitedDFS)
-    bw.write("\n")
+    visited = BooleanArray(n + 1)
+    bw.write(dfs(v, graph, visited) + "\n")
 
-    val visitedBFS = BooleanArray(n + 1)
-    bfs(v, graph, visitedBFS)
+    visited = BooleanArray(n + 1)
+    bw.write(bfs(v, graph, visited) + "\n")
 
     bw.flush()
     bw.close()
+}
+
+private fun dfs(node: Int, graph: Array<MutableList<Int>>, visited: BooleanArray): String {
+    val result = StringBuilder()
+
+    fun dfsHelper(current: Int) {
+        visited[current] = true
+        result.append("$current ")
+
+        for (next in graph[current]) {
+            if (visited[next].not()) {
+                dfsHelper(next)
+            }
+        }
+    }
+    dfsHelper(node)
+
+    return result.toString()
+}
+
+private fun bfs(start: Int, graph: Array<MutableList<Int>>, visited: BooleanArray): String {
+    val queue: Queue<Int> = LinkedList()
+    val result = StringBuilder()
+
+    queue.add(start)
+    visited[start] = true
+
+    while (queue.isNotEmpty()) {
+        val current = queue.poll()
+        result.append("$current ")
+
+        for (next in graph[current]) {
+            if (visited[next].not()) {
+                queue.offer(next)
+                visited[next] = true
+            }
+        }
+    }
+
+    return result.toString()
 }
