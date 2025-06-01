181326e4cc25fdabb0f92634bccf7d6de9af4619
Sera Heo <quessr00@gmail.com>
2025-06-01 22:48:27 +0900

    [Bronze II] Title: 음계, Time: 120 ms, Memory: 20848 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.kt
+++ 백준/Bronze/2920. 음계/음계.kt
@@ -2,11 +2,9 @@ fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
 
-    val input = br.readLine().split(" ")
-
-    when (input.joinToString(" ")) {
-        input.sorted().joinToString(" ") -> bw.write("ascending")
-        input.sortedDescending().joinToString(" ") -> bw.write("descending")
+    when (br.readLine().split(" ").joinToString(" ")) {
+        "1 2 3 4 5 6 7 8" -> bw.write("ascending")
+        "8 7 6 5 4 3 2 1" -> bw.write("descending")
         else -> bw.write("mixed")
     }
 
