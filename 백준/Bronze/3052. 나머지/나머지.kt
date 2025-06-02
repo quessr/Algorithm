1547501f0fec9406a171da87073ca8b45df7d631
Sera Heo <quessr00@gmail.com>
2025-06-02 23:34:42 +0900

    [Bronze II] Title: 나머지, Time: 104 ms, Memory: 14972 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.kt
+++ 백준/Bronze/3052. 나머지/나머지.kt
@@ -2,9 +2,9 @@ fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
 
-    val numList = Array(10) { br.readLine() }
+    val numList = Array(10) { br.readLine().toInt() }
 
-    bw.write("${numList.map { it.toInt() % 42 }.toSet().count()}")
+    bw.write("${numList.map { it % 42 }.toSet().size}")
 
     bw.flush()
     bw.close()
